1728fc56e12d991b946e74886e359d6a717917eb
Federico Frontini <frontinifederico95@gmail.com>
2023-12-07 19:45:47 +0100

    fix: leading zeroes

--- src/italian_holidays/italian_holidays.py
+++ src/italian_holidays/italian_holidays.py
@@ -12,7 +12,7 @@ class ItalianHolidays():
 	
 	def __init__(self, custom_holidays: list = []):
 
-		self._custom_holidays: list = custom_holidays
+		self._custom_holidays: list[str | datetime] = custom_holidays
 
 	def is_holiday(self, date: datetime) -> bool:
 		"""Method tha determines whether a given day is a holiday
@@ -24,8 +24,6 @@ class ItalianHolidays():
 		"""
 		# from the year in the date passed to the caller 
 		# we add easter related holidays to the dict
-		self._add_easter_holidays(year=int(date.year))
-
 		if not isinstance(date, str) and not isinstance(date, datetime):
 			raise TypeError('is_holiday can accept only string or datetime object')
 		date_obj: datetime | str = date
@@ -34,6 +32,7 @@ class ItalianHolidays():
 				date_obj = datetime.strptime(date, '%Y-%m-%d')
 			except ValueError:
 				raise ValueError('date must be in the format \'Y-m-d\'')
+		self._add_easter_holidays(year=int(date_obj.year))
 		# we then check whether the date is in there
 		date_str: str = f"{date_obj.month}-{date_obj.day}"
 		if date_str in holidays_dict:
@@ -71,8 +70,10 @@ class ItalianHolidays():
 					return 'Custom holiday'
 		return name
 
-	def _easter(self, year: str) -> str:
+	def _easter(self, year: int) -> str:
 		"""Implementation of the Gauss algorithm to find Easter
+
+		Doesn't work before 1582!
 
 		:param year: the year to check
 		:type year: str
@@ -94,31 +95,37 @@ class ItalianHolidays():
 		# A corner case,
 		# when D is 29
 		if ((D == 29) and (E == 6)):
-			easter = year + "-04-19"
+			easter = str(year) + "-04-19"
 		# Another corner case,
 		# when D is 28
 		elif ((D == 28) and (E == 6)):
-			easter = year + "-04-18"
+			easter = str(year) + "-04-18"
 		else:
 			# If days > 31, move to April
 			# April = 4th Month
 			if (days > 31):
-				easter = year + "-04-" + (days - 31)
+				easter = str(year) + "-04-" + str(days - 31)
 			else:
 				# Otherwise, stay on March
 				# March = 3rd Month
-				easter = year +"-03-" + days
-		return easter
+				easter = year +"-03-" + str(days)
+		easter_datetime: datetime = datetime.strptime(easter, "%Y-%m-%d")
+		return easter_datetime.strftime("%Y-%#m-%#d")
+
 
 	def _easter_monday(self, year) -> str:
 		easter: str = self._easter(year)
 		easter_monday_date: datetime = datetime.strptime(easter, "%Y-%m-%d") + timedelta(days=1)
-		return easter_monday_date.strftime("%Y-%m-%d")
+		return easter_monday_date.strftime("%Y-%#m-%#d")
 	
 	def _add_easter_holidays(self, year: str) -> None:
 		easter: str = self._easter(year=year)
 		easter_monday: str = self._easter_monday(year=year)
-		easter = easter[:-5]
-		easter_monday = easter_monday[:-5]
+		easter = easter[5:]
+		easter_monday = easter_monday[5:]
 		holidays_dict[easter] = "Easter Sunday"
 		holidays_dict[easter_monday] = "Easter Monday"
+
+holydays = ItalianHolidays()
+print(holydays.is_holiday('1787-01-01'))
+print(holydays.holiday_name('1061-12-8'))
